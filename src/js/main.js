1469b5205bc3a6ff57b61303cf847433feb01857
Gosia Siłkowska <silkowska.m@gmail.com>
2020-05-12 19:39:34 +0200

    feat: updated images

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -17,13 +17,17 @@ if ('serviceWorker' in navigator) {
 const reposList = document.querySelector(".projects-content__list--js");
 
 fetch("https://api.github.com/users/gorzauka/repos?sort=updated")
-.then(resp => resp.json())
-.then(resp => {
-const repos = resp;
-for (const repo of repos){
-  reposList.innerHTML += `<li class="projects-content__item"><a class="projects-content__link" href='${repo.html_url}'>${repo.name}</li>`;
-}
-})
+  .then(resp => resp.json())
+  .then(resp => {
+    const repos = resp;
+    for (const repo of repos) {
+      const { description, html_url, name } = repo;
+      reposList.innerHTML += `
+      <li class="projects-content__item">
+      <a class="projects-content__link" href='${repo.html_url}'>${repo.name}
+    </li>`
+    }
+  })
 
 
 
